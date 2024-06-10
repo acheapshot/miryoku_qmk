33e2711c70f5d202bb272d2d3df9fbfc7c88b014
acheapshot <66885939+acheapshot@users.noreply.github.com>
2024-06-09 20:45:57 -0500

    Update manna-harbour_miryoku.c

--- users/manna-harbour_miryoku/manna-harbour_miryoku.c
+++ users/manna-harbour_miryoku/manna-harbour_miryoku.c
@@ -59,62 +59,62 @@ const key_override_t **key_overrides = (const key_override_t *[]){
 };
 
 
-//acheapshot combos
+//acheapshot + Miryoku combos
+#include <stdint.h>
+
 enum combo_events {
-	UH_GOODDAY,
-	IH_BRMC
+    UH_GOODDAY,
+    IH_BRMC,
+    THUMBCOMBOS_BASE_RIGHT,
+    THUMBCOMBOS_BASE_LEFT,
+    THUMBCOMBOS_NAV,
+    THUMBCOMBOS_MOUSE,
+    THUMBCOMBOS_MEDIA,
+    THUMBCOMBOS_NUM,
+    THUMBCOMBOS_SYM,
+    THUMBCOMBOS_FUN
 };
 
 const uint16_t PROGMEM good_day_combo[] = {KC_U, KC_H, COMBO_END};
 const uint16_t PROGMEM brmc_combo[] = {KC_I, KC_H, COMBO_END};
-
-combo_t key_combos[] = {
-  [UH_GOODDAY] = COMBO_ACTION(good_day_combo),
-  [IH_BRMC] = COMBO_ACTION(brmc_combo),
-};
-
-void process_combo_event(uint16_t combo_index, bool pressed) {
-  switch(combo_index) {
-    case UH_GOODDAY:
-      if (pressed) {
-        SEND_STRING("Good Day,/n/n");
-      }
-      break;
-    case IH_BRMC:
-      if (pressed) {
-        SEND_STRING("Best regards,/nMatt Chesser");
-      }
-      break;
-  }
-}
-
-// thumb combos
-
-#if defined (MIRYOKU_KLUDGE_THUMBCOMBOS)
 const uint16_t PROGMEM thumbcombos_base_right[] = {LT(U_SYM, KC_ENT), LT(U_NUM, KC_BSPC), COMBO_END};
 const uint16_t PROGMEM thumbcombos_base_left[] = {LT(U_NAV, KC_SPC), LT(U_MOUSE, KC_TAB), COMBO_END};
 const uint16_t PROGMEM thumbcombos_nav[] = {KC_ENT, KC_BSPC, COMBO_END};
 const uint16_t PROGMEM thumbcombos_mouse[] = {KC_BTN2, KC_BTN1, COMBO_END};
 const uint16_t PROGMEM thumbcombos_media[] = {KC_MSTP, KC_MPLY, COMBO_END};
 const uint16_t PROGMEM thumbcombos_num[] = {KC_0, KC_MINS, COMBO_END};
-  #if defined (MIRYOKU_LAYERS_FLIP)
+#if defined(MIRYOKU_LAYERS_FLIP)
 const uint16_t PROGMEM thumbcombos_sym[] = {KC_UNDS, KC_LPRN, COMBO_END};
-  #else
+#else
 const uint16_t PROGMEM thumbcombos_sym[] = {KC_RPRN, KC_UNDS, COMBO_END};
-  #endif
-const uint16_t PROGMEM thumbcombos_fun[] = {KC_SPC, KC_TAB, COMBO_END};
-combo_t key_combos[COMBO_COUNT] = {
-  COMBO(thumbcombos_base_right, LT(U_FUN, KC_DEL)),
-  COMBO(thumbcombos_base_left, LT(U_MEDIA, KC_ESC)),
-  COMBO(thumbcombos_nav, KC_DEL),
-  COMBO(thumbcombos_mouse, KC_BTN3),
-  COMBO(thumbcombos_media, KC_MUTE),
-  COMBO(thumbcombos_num, KC_DOT),
-  #if defined (MIRYOKU_LAYERS_FLIP)
-  COMBO(thumbcombos_sym, KC_RPRN),
-  #else
-  COMBO(thumbcombos_sym, KC_LPRN),
-  #endif
-  COMBO(thumbcombos_fun, KC_APP)
-};
 #endif
+const uint16_t PROGMEM thumbcombos_fun[] = {KC_SPC, KC_TAB, COMBO_END};
+
+combo_t key_combos[] = {
+    [UH_GOODDAY] = COMBO_ACTION(good_day_combo),
+    [IH_BRMC] = COMBO_ACTION(brmc_combo),
+    [THUMBCOMBOS_BASE_RIGHT] = COMBO(thumbcombos_base_right, LT(U_FUN, KC_DEL)),
+    [THUMBCOMBOS_BASE_LEFT] = COMBO(thumbcombos_base_left, LT(U_MEDIA, KC_ESC)),
+    [THUMBCOMBOS_NAV] = COMBO(thumbcombos_nav, KC_DEL),
+    [THUMBCOMBOS_MOUSE] = COMBO(thumbcombos_mouse, KC_BTN3),
+    [THUMBCOMBOS_MEDIA] = COMBO(thumbcombos_media, KC_MUTE),
+    [THUMBCOMBOS_NUM] = COMBO(thumbcombos_num, KC_TRNS),
+    [THUMBCOMBOS_SYM] = COMBO(thumbcombos_sym, KC_TRNS),
+    [THUMBCOMBOS_FUN] = COMBO(thumbcombos_fun, KC_TRNS),
+};
+
+void process_combo_event(uint16_t combo_index, bool pressed) {
+    switch (combo_index) {
+        case UH_GOODDAY:
+            if (pressed) {
+                SEND_STRING("Good Day,\n\n");
+            }
+            break;
+        case IH_BRMC:
+            if (pressed) {
+                SEND_STRING("Best regards,\nMatt Chesser");
+            }
+            break;
+        // Add cases for other combo indexes as needed
+    }
+}
